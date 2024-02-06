5119074b09da65dff183fa987188a46f0c3fee81
zulalsarioglu <zulal.sariogluu@gmail.com>
2024-02-06 20:31:15 +0300

    Initial Commit

---
+++ NewsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsDemo
+//
+//  Created by Zülal Sarıoğlu on 6.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
